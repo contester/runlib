4bf7fdfcbde0366f83fbbaeca621bc6759e189f6
Paul Komkoff <i@stingr.net>
2012-08-12 18:34:50 +0100

    .

--- sub32/win32.go
+++ sub32/win32.go
@@ -2,7 +2,7 @@ package sub32
 
 import (
   "syscall"
-//  "unsafe"
+  "unsafe"
 )
 
 var (
@@ -15,6 +15,13 @@ type Environment interface {}
 type StartupInfo interface {}
 type ProcessInformation interface {}
 
+func nilOrString(src *string) (result *uint16) {
+  if src != nil {
+    return syscall.StringToUTF16Ptr(*src)
+  }
+  return nil
+}
+
 func CreateProcessWithLogonW(
     username string,
     domain *string,
@@ -22,10 +29,37 @@ func CreateProcessWithLogonW(
     logonFlags uint32,
     applicationName *string,
     commandLine *string,
-    creationFlags *uint32,
+    creationFlags uint32,
     environment *Environment,
     currentDirectory *string,
-    startupInfo *StartupInfo) (pi *ProcessInformation, err error) {
+    startupInfo *StartupInfo) (processInformation *ProcessInformation, err error) {
+
+  pUsername := syscall.StringToUTF16Ptr(username)
+  pDomain := nilOrString(domain)
+  pPassword := syscall.StringToUTF16Ptr(password)
+  pApplicationName := nilOrString(applicationName)
+  pCommandLine := nilOrString(commandLine)
+  pCurrentDirectory := nilOrString(currentDirectory)
+  
+  // si := &syscall.StartupInfo{}
+  pi := &syscall.ProcessInformation{}
+
+  r1, _, e1 := procCreateProcessWithLogonW.Call(
+    uintptr(unsafe.Pointer(pUsername)),
+    uintptr(unsafe.Pointer(pDomain)),
+    uintptr(unsafe.Pointer(pPassword)),
+    uintptr(logonFlags),
+    uintptr(unsafe.Pointer(pApplicationName)),
+    uintptr(unsafe.Pointer(pCommandLine)),
+    uintptr(creationFlags),
+    uintptr(0), // env
+    uintptr(unsafe.Pointer(pCurrentDirectory)),
+    uintptr(0), // si
+    uintptr(unsafe.Pointer(pi)))
+
+  if int(r1) == 0 {
+    return nil, e1
+  }
 
   return nil, nil
 }

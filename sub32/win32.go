c88881a78ceb5103b50dd7968372351345540069
Paul Komkoff <i@stingr.net>
2012-08-14 00:19:29 +0100

    .

--- sub32/win32.go
+++ sub32/win32.go
@@ -1,78 +1,78 @@
 package sub32
 
 import (
-  "syscall"
-  "unsafe"
+	"syscall"
+	"unsafe"
 )
 
 var (
-  advapi32 = syscall.NewLazyDLL("advapi32.dll")
-  kernel32 = syscall.NewLazyDLL("kernel32.dll")
-  psapi = syscall.NewLazyDLL("psapi.dll")
+	advapi32 = syscall.NewLazyDLL("advapi32.dll")
+	kernel32 = syscall.NewLazyDLL("kernel32.dll")
+	psapi    = syscall.NewLazyDLL("psapi.dll")
 
-  procCreateProcessWithLogonW = advapi32.NewProc("CreateProcessWithLogonW")
-  procCreateProcessW = kernel32.NewProc("CreateProcessW")
-  procResumeThread = kernel32.NewProc("ResumeThread")
-  procGetProcessMemoryInfo = psapi.NewProc("GetProcessMemoryInfo")
+	procCreateProcessWithLogonW = advapi32.NewProc("CreateProcessWithLogonW")
+	procCreateProcessW          = kernel32.NewProc("CreateProcessW")
+	procResumeThread            = kernel32.NewProc("ResumeThread")
+	procGetProcessMemoryInfo    = psapi.NewProc("GetProcessMemoryInfo")
 )
 
 const (
-  CREATE_BREAKAWAY_FROM_JOB = 0x01000000
-  CREATE_NEW_CONSOLE = 0x00000010
-  CREATE_NEW_PROCESS_GROUP = 0x00000200
-  CREATE_SUSPENDED = 0x00000004
+	CREATE_BREAKAWAY_FROM_JOB = 0x01000000
+	CREATE_NEW_CONSOLE        = 0x00000010
+	CREATE_NEW_PROCESS_GROUP  = 0x00000200
+	CREATE_SUSPENDED          = 0x00000004
 
-  LOGON_WITH_PROFILE = 0x00000001
+	LOGON_WITH_PROFILE = 0x00000001
 
-  STARTF_FORCEOFFFEEDBACK = 0x00000080
+	STARTF_FORCEOFFFEEDBACK = 0x00000080
 
-  FILE_FLAG_SEQUENTIAL_SCAN = 0x08000000
+	FILE_FLAG_SEQUENTIAL_SCAN = 0x08000000
 )
 
 type ProcessMemoryCountersEx struct {
-  Cb uint32
-  PageFaultCount uint32
-  PeakWorkingSetSize uint32
-  WorkingSetSize uint32
-  QuotaPeakPagedPoolUsage uint32
-  QuotaPagedPoolUsage uint32
-  QuotaPeakNonPagedPoolUsage uint32
-  QuotaNonPagedPoolUsage uint32
-  PagefileUsage uint32
-  PeakPagefileUsage uint32
-  PrivateUsage uint32
+	Cb                         uint32
+	PageFaultCount             uint32
+	PeakWorkingSetSize         uint32
+	WorkingSetSize             uint32
+	QuotaPeakPagedPoolUsage    uint32
+	QuotaPagedPoolUsage        uint32
+	QuotaPeakNonPagedPoolUsage uint32
+	QuotaNonPagedPoolUsage     uint32
+	PagefileUsage              uint32
+	PeakPagefileUsage          uint32
+	PrivateUsage               uint32
 }
 
 func StringPtrToUTF16Ptr(src *string) (result *uint16) {
-  if src != nil {
-    return syscall.StringToUTF16Ptr(*src)
-  }
-  return nil
+	if src != nil {
+		return syscall.StringToUTF16Ptr(*src)
+	}
+	return nil
 }
 
 func ListToEnvironmentBlock(list *[]string) *uint16 {
-  if (list == nil) {
-    return nil
-  }
+	if list == nil {
+		return nil
+	}
 
-  size := 1
-  for _, v := range *list {
-    size += len(syscall.StringToUTF16(v))
-  }
+	size := 1
+	for _, v := range *list {
+		size += len(syscall.StringToUTF16(v))
+	}
 
-  result := make([]uint16, size)
+	result := make([]uint16, size)
 
-  tail := 0
+	tail := 0
 
-  for _, v := range *list {
-    uline := syscall.StringToUTF16(v)
-    copy(result[tail:], uline)
-    tail += len(uline)
-  }
+	for _, v := range *list {
+		uline := syscall.StringToUTF16(v)
+		copy(result[tail:], uline)
+		tail += len(uline)
+	}
 
-  result[tail] = 0
+	result[tail] = 0
 
-  return &result[0]
+	return &result[0]
 }
 
 /*
@@ -98,135 +98,52 @@ func getEnvBlock(env Environment) *uint16 {
 */
 
 func CreateProcessWithLogonW(
-    username *uint16,
-    domain *uint16,
-    password *uint16,
-    logonFlags uint32,
-    applicationName *uint16,
-    commandLine *uint16,
-    creationFlags uint32,
-    environment *uint16,
-    currentDirectory *uint16,
-    startupInfo *syscall.StartupInfo,
-    processInformation *syscall.ProcessInformation) (err error) {
+	username *uint16,
+	domain *uint16,
+	password *uint16,
+	logonFlags uint32,
+	applicationName *uint16,
+	commandLine *uint16,
+	creationFlags uint32,
+	environment *uint16,
+	currentDirectory *uint16,
+	startupInfo *syscall.StartupInfo,
+	processInformation *syscall.ProcessInformation) (err error) {
 
-  r1, _, e1 := procCreateProcessWithLogonW.Call(
-    uintptr(unsafe.Pointer(username)),
-    uintptr(unsafe.Pointer(domain)),
-    uintptr(unsafe.Pointer(password)),
-    uintptr(logonFlags),
-    uintptr(unsafe.Pointer(applicationName)),
-    uintptr(unsafe.Pointer(commandLine)),
-    uintptr(creationFlags),
-    uintptr(unsafe.Pointer(environment)), // env
-    uintptr(unsafe.Pointer(currentDirectory)),
-    uintptr(unsafe.Pointer(startupInfo)),
-    uintptr(unsafe.Pointer(processInformation)))
+	r1, _, e1 := procCreateProcessWithLogonW.Call(
+		uintptr(unsafe.Pointer(username)),
+		uintptr(unsafe.Pointer(domain)),
+		uintptr(unsafe.Pointer(password)),
+		uintptr(logonFlags),
+		uintptr(unsafe.Pointer(applicationName)),
+		uintptr(unsafe.Pointer(commandLine)),
+		uintptr(creationFlags),
+		uintptr(unsafe.Pointer(environment)), // env
+		uintptr(unsafe.Pointer(currentDirectory)),
+		uintptr(unsafe.Pointer(startupInfo)),
+		uintptr(unsafe.Pointer(processInformation)))
 
-  if int(r1) == 0 {
-    return e1
-  }
+	if int(r1) == 0 {
+		return e1
+	}
 
-  return nil
+	return nil
 }
 
 func ResumeThread(thread syscall.Handle) (suspendCount int, err error) {
-  r1, _, e1 := procResumeThread.Call(uintptr(thread))
-  if int(r1) == -1 {
-    return -1, e1
-  }
-  return int(r1), nil
+	r1, _, e1 := procResumeThread.Call(uintptr(thread))
+	if int(r1) == -1 {
+		return -1, e1
+	}
+	return int(r1), nil
 }
 
 func GetProcessMemoryInfo(process syscall.Handle) (pmc *ProcessMemoryCountersEx, err error) {
-  pmc = &ProcessMemoryCountersEx{}
-  pmc.Cb = uint32(unsafe.Sizeof(*pmc))
-  r1, _, e1 := procGetProcessMemoryInfo.Call(uintptr(process), uintptr(unsafe.Pointer(pmc)), uintptr(pmc.Cb))
-  if int(r1) == 0 {
-    return nil, e1
-  }
-  return pmc, nil
+	pmc = &ProcessMemoryCountersEx{}
+	pmc.Cb = uint32(unsafe.Sizeof(*pmc))
+	r1, _, e1 := procGetProcessMemoryInfo.Call(uintptr(process), uintptr(unsafe.Pointer(pmc)), uintptr(pmc.Cb))
+	if int(r1) == 0 {
+		return nil, e1
+	}
+	return pmc, nil
 }
-
-
-/*
-func CreateProcessWithLogonWWWW(
-    username string,
-    domain *string,
-    password string,
-    logonFlags uint32,
-    applicationName *string,
-    commandLine *string,
-    creationFlags uint32,
-    environment Environment,
-    currentDirectory *string,
-    startupInfo *StartupInfo) (processInformation *ProcessInformation, err error) {
-
-  pUsername := syscall.StringToUTF16Ptr(username)
-  pDomain := nilOrString(domain)
-  pPassword := syscall.StringToUTF16Ptr(password)
-  pApplicationName := nilOrString(applicationName)
-  pCommandLine := nilOrString(commandLine)
-  pCurrentDirectory := nilOrString(currentDirectory)
-  
-  // si := &syscall.StartupInfo{}
-  pi := &syscall.ProcessInformation{}
-
-  // testBlock(getEnvBlock(environment))
-
-  r1, _, e1 := procCreateProcessWithLogonW.Call(
-    uintptr(unsafe.Pointer(pUsername)),
-    uintptr(unsafe.Pointer(pDomain)),
-    uintptr(unsafe.Pointer(pPassword)),
-    uintptr(LOGON_WITH_PROFILE),
-    uintptr(unsafe.Pointer(pApplicationName)),
-    uintptr(unsafe.Pointer(pCommandLine)),
-    uintptr(syscall.CREATE_UNICODE_ENVIRONMENT), //creationFlags),
-    uintptr(unsafe.Pointer(getEnvBlock(environment))), // env
-    uintptr(unsafe.Pointer(pCurrentDirectory)),
-    uintptr(0), // si
-    uintptr(unsafe.Pointer(pi)))
-
-  if int(r1) == 0 {
-    return nil, e1
-  }
-
-  return nil, nil
-}
-    
-func CreateProcessWWWW(
-    applicationName *string,
-    commandLine *string,
-    environment Environment,
-    currentDirectory *string,
-    startupInfo StartupInfo) (processInformation *ProcessInformation, err error) {    
-
-  pApplicationName := nilOrString(applicationName)
-  pCommandLine := nilOrString(commandLine)
-  pCurrentDirectory := nilOrString(currentDirectory)
-
-  pi := &syscall.ProcessInformation{}
-  si := &syscall.StartupInfo{}
-  si.Cb = uint32(unsafe.Sizeof(*si))
-  // si.Flags = syscall.STARTF_USESTDHANDLES
-
-  r1, _, e1 := procCreateProcessW.Call(
-    uintptr(unsafe.Pointer(pApplicationName)),
-    uintptr(unsafe.Pointer(pCommandLine)),
-    uintptr(0),
-    uintptr(0),
-    uintptr(1),
-    uintptr(CREATE_NEW_PROCESS_GROUP | CREATE_NEW_CONSOLE | syscall.CREATE_UNICODE_ENVIRONMENT | CREATE_BREAKAWAY_FROM_JOB),
-    uintptr(unsafe.Pointer(getEnvBlock(environment))), // env
-    uintptr(unsafe.Pointer(pCurrentDirectory)),
-    uintptr(unsafe.Pointer(si)),
-    uintptr(unsafe.Pointer(pi)))
-
-  if int(r1) == 0 {
-    return nil, e1
-  }
-
-  return nil, nil
-}    
-    
-*/

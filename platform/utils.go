856cd609664ec2d8f777e96e11c6c550fd20abb7
Paul Komkoff <i@stingr.net>
2012-08-23 23:04:27 +0100

    acl

---
+++ platform/utils.go
@@ -0,0 +1,11 @@
+package platform
+
+import (
+	"unsafe"
+)
+
+func AlignedBuffer(size, offset int) []byte {
+	buf := make([]byte, size + offset)
+	ofs := int((uintptr(offset) - (uintptr(unsafe.Pointer(&buf[0])) % uintptr(offset))) % uintptr(offset))
+	return buf[ofs:ofs+size]
+}

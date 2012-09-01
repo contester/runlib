413b1349adf3c2a55a52e4f3cc7a01782fb69434
Paul P. Komkoff <i@stingr.net>
2012-09-02 00:12:33 +0400

    It now builds on linux

--- service/service_linux.go
+++ service/service_linux.go
@@ -3,8 +3,29 @@
 package service
 
 import (
+	"code.google.com/p/goconf/conf"
+	"strconv"
 )
+
+const PLATFORM_ID = "linux"
 
 func OnOsCreateError(err error) (bool, error) {
 	return false, err
+}
+
+func getPasswords(c *conf.ConfigFile) ([]string, error) {
+	count, err := c.GetInt("default", "sandboxCount")
+	if err != nil {
+		return nil, err
+	}
+	result := make([]string, count)
+	for i := range result {
+		result[i] = "password" + strconv.Itoa(i)
+	}
+	return result, nil
+}
+
+func setAcl(path, username string) error {
+	// TODO: use setfacl to set acl
+	return nil
 }
--- subprocess/subprocess_linux.go
+++ subprocess/subprocess_linux.go
@@ -1,6 +1,9 @@
-// +build linux
-
 package subprocess
+
+import (
+	"os/user"
+	"strconv"
+)
 
 type LoginInfo struct {
 	Uid int
@@ -10,4 +13,18 @@ type PlatformOptions struct {}
 
 type PlatformData struct {
 	Pid int
+}
+
+func NewLoginInfo(username, password string) (*LoginInfo, error) {
+	u, err := user.Lookup(username)
+	if err != nil {
+		return nil, err
+	}
+	uid, err := strconv.Atoi(u.Uid)
+	if err != nil {
+		return nil, err
+	}
+	return &LoginInfo{
+		Uid: uid,
+	}, nil
 }

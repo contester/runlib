294112a8960a05eac7b55300b6e931fef5ba2202
Paul Komkoff <i@stingr.net>
2012-09-02 11:08:51 +0100

    Keep only minimum in files that import C

---
+++ linux/clone_other.go
@@ -0,0 +1,48 @@
+package linux
+
+import (
+	"os"
+	"syscall"
+	"fmt"
+)
+
+type StdHandles struct {
+	StdIn, StdOut, StdErr *os.File
+}
+
+func (s *StdHandles) Close() {
+	s.StdIn.Close()
+	s.StdOut.Close()
+	s.StdErr.Close()
+}
+
+func (c *CloneParams) Clone() (int, error) {
+	pid := callClone(c)
+	// TODO: clone errors?
+	c.CommWriter.Close()
+	c.stdhandles.Close()
+
+	var status syscall.WaitStatus
+	for {
+		wpid, err := syscall.Wait4(pid, &status, 0, nil) // TODO: rusage
+		if err != nil {
+			return -1, err
+		}
+		if wpid == pid {
+			break
+		}
+	}
+	if status.Stopped() && status.StopSignal() == syscall.SIGTRAP {
+		// cgroup attach
+		err := syscall.PtraceDetach(pid)
+		if err != nil {
+			// wtf to do here
+		}
+		return pid, nil
+	}
+	err := syscall.Kill(pid, syscall.SIGKILL)
+	if err != nil {
+		return -1, err
+	}
+	return -1, fmt.Errorf("traps, signals, dafuq is this")
+}
--- linux/cloneopt.go
+++ linux/cloneopt.go
@@ -13,17 +13,6 @@ import "runlib/tools"
 import "os"
 import "runtime"
 import "syscall"
-import "fmt"
-
-type StdHandles struct {
-	StdIn, StdOut, StdErr *os.File
-}
-
-func (s *StdHandles) Close() {
-	s.StdIn.Close()
-	s.StdOut.Close()
-	s.StdErr.Close()
-}
 
 type CloneParams struct {
 	repr C.struct_CloneParams
@@ -114,33 +103,7 @@ func freeCloneParams(s *CloneParams) {
 	}
 }
 
-func (c *CloneParams) Clone() (int, error) {
-	pid := int(C.Clone(&c.repr))
-	// TODO: clone errors?
-	c.CommWriter.Close()
-	c.stdhandles.Close()
-	
-	var status syscall.WaitStatus
-	for {
-		wpid, err := syscall.Wait4(pid, &status, 0, nil) // TODO: rusage
-		if err != nil {
-			return -1, err
-		}
-		if wpid == pid {
-			break
-		}
-	}
-	if status.Stopped() && status.StopSignal() == syscall.SIGTRAP {
-		// cgroup attach
-		err := syscall.PtraceDetach(pid)
-		if err != nil {
-			// wtf to do here
-		}
-		return pid, nil
-	}
-	err := syscall.Kill(pid, syscall.SIGKILL)
-	if err != nil {
-		return -1, err
-	}
-	return -1, fmt.Errorf("traps, signals, dafuq is this")
+func callClone(c *CloneParams) int {
+	return int(C.Clone(&c.repr))
 }
+

673240e59d03a9fc8bde1321341316c889842e7b
Paul Komkoff <i@stingr.net>
2013-01-08 13:09:10 +0000

    Implement hashing option in stat

--- service/fileops.go
+++ service/fileops.go
@@ -2,25 +2,38 @@ package service
 
 import (
 	"code.google.com/p/goprotobuf/proto"
-	l4g "code.google.com/p/log4go"
 	"os"
+	"syscall"
 	"path/filepath"
 	"runlib/contester_proto"
 	"crypto/sha1"
 	"io"
 )
 
-func statFile(name string) (*contester_proto.FileStat, error) {
+func statFile(name string, hash_it bool) (*contester_proto.FileStat, error) {
 	result := &contester_proto.FileStat{}
 	result.Name = &name
 	info, err := os.Stat(name)
 	if err != nil {
-		return nil, err
+		// Handle ERROR_PATH_NOT_FOUND - return no error and nil instead of stat struct
+		if path_err, ok := err.(*os.PathError); ok {
+			if errno, ok := path_err.Err.(syscall.Errno); ok && errno == syscall.Errno(syscall.ERROR_PATH_NOT_FOUND) {
+				return nil, nil
+			}
+		}
+
+		return nil, NewServiceError("os.Stat", err)
 	}
 	if info.IsDir() {
 		result.IsDirectory = proto.Bool(true)
 	} else {
 		result.Size = proto.Uint64(uint64(info.Size()))
+		if hash_it {
+			result.Sha1Sum, err = hashFile(name)
+			if err != nil {
+				return nil, NewServiceError("hashFile", err)
+			}
+		}
 	}
 	return result, nil
 }
@@ -49,8 +62,7 @@ func (s *Contester) Stat(request *contester_proto.StatRequest, response *contest
 	if request.SandboxId != nil {
 		sandbox, err := getSandboxById(s.Sandboxes, *request.SandboxId)
 		if err != nil {
-			l4g.Error(err)
-			return err
+			return NewServiceError("getSandboxById", err)
 		}
 		sandbox.Mutex.RLock()
 		defer sandbox.Mutex.RUnlock()
@@ -60,22 +72,23 @@ func (s *Contester) Stat(request *contester_proto.StatRequest, response *contest
 	for _, name := range request.Name {
 		resolved, _, err := resolvePath(s.Sandboxes, name, false)
 		if err != nil {
-			l4g.Error(err)
-			continue
+			return NewServiceError("resolvePath", err)
 		}
 		var expanded []string
-		if request.Expand != nil && *request.Expand {
+		if request.GetExpand() {
 			expanded, err = filepath.Glob(resolved)
 			if err != nil {
-				l4g.Error("Expanding", resolved, err)
-				continue
+				return NewServiceError("filepath.Glob", err)
 			}
 		} else {
 			expanded = []string{resolved}
 		}
 
 		for _, name := range expanded {
-			stat, _ := statFile(name)
+			stat, err := statFile(name, request.GetCalculateSha1())
+			if err != nil {
+				return NewServiceError("statFile", err)
+			}
 			if stat != nil {
 				response.Stats = append(response.Stats, stat)
 			}

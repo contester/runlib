5651303fd76406e76755f2dd2920f35ff774a5d7
Paul Komkoff <i@stingr.net>
2015-06-02 22:18:47 +0100

    ...

--- win32/win32_windows.go
+++ win32/win32_windows.go
@@ -104,38 +104,6 @@ type OSVersionInfoEx struct {
 	Reserve           byte
 }
 
-func unpackConditionMask(cm uint64) (m1, m2 uintptr) {
-	return uintptr(cm & 0xffffffff), uintptr(cm >> 32)
-}
-
-func packConditionMask(m1, m2 uintptr) uint64 {
-	return uint64(m1) | (uint64(m2) << 32)
-}
-
-func VerSetConditionMask(lConditionMask uint64, typeBitMask uint32, conditionMask uint8) uint64 {
-	m1, m2 := unpackConditionMask(lConditionMask)
-
-	fmt.Println("d:", lConditionMask, m1, m2, typeBitMask, conditionMask)
-	r1, r2, e1 := procVerSetConditionMask.Call(m1, m2, uintptr(typeBitMask), uintptr(conditionMask))
-	fmt.Printf("x:", r1, r2, e1)
-	return packConditionMask(r1, r2)
-}
-
-func VerifyWindowsInfoW(vi OSVersionInfoEx, typeMask uint32, conditionMask uint64) (bool, error) {
-	m1, m2 := unpackConditionMask(conditionMask)
-	vi.OSVersionInfoSize = uint32(unsafe.Sizeof(vi))
-
-	r1, r2, e1 := procVerifyVersionInfoW.Call(uintptr(unsafe.Pointer(&vi)), uintptr(typeMask), m1, m2)
-	fmt.Printf("v: %s, %s, %s\n", r1, r2, e1)
-	if r1 != 0 {
-		return true, nil
-	}
-	if r1 == 0 && e1 == ERROR_OLD_WIN_VERSION {
-		return false, nil
-	}
-	return false, os.NewSyscallError("VerifyVersionInfoW", e1)
-}
-
 func IsWindows8OrGreater() bool {
 	cm := VerSetConditionMask(0, VER_MAJORVERSION, VER_GREATER)
 	cm = VerSetConditionMask(cm, VER_MINORVERSION, VER_GREATER_EQUAL)
--- win32/win32_windows_amd64.go
+++ win32/win32_windows_amd64.go
@@ -1,4 +1,8 @@
 package win32
+import (
+	"unsafe"
+	"fmt"
+	"os")
 
 type JobObjectExtendedLimitInformation struct {
 	BasicLimitInformation JobObjectBasicLimitInformation
@@ -7,4 +11,23 @@ type JobObjectExtendedLimitInformation struct {
 	JobMemoryLimit        uintptr // SIZE_T
 	PeakProcessMemoryUsed uintptr // SIZE_T
 	PeakJobMemoryUsed     uintptr // SIZE_T
+}
+
+func VerSetConditionMask(lConditionMask uint64, typeBitMask uint32, conditionMask uint8) uint64 {
+	r1, _, _ := procVerSetConditionMask.Call(uintptr(lConditionMask), uintptr(typeBitMask), uintptr(conditionMask))
+	return uint64(r1)
+}
+
+func VerifyWindowsInfoW(vi OSVersionInfoEx, typeMask uint32, conditionMask uint64) (bool, error) {
+	vi.OSVersionInfoSize = uint32(unsafe.Sizeof(vi))
+
+	r1, _, e1 := procVerifyVersionInfoW.Call(uintptr(unsafe.Pointer(&vi)), uintptr(typeMask), uintptr(conditionMask))
+	fmt.Printf("v: %s, %s\n", r1, e1)
+	if r1 != 0 {
+		return true, nil
+	}
+	if r1 == 0 && e1 == ERROR_OLD_WIN_VERSION {
+		return false, nil
+	}
+	return false, os.NewSyscallError("VerifyVersionInfoW", e1)
 }

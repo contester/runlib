f52c8aeb6d70e25bc9111f0bf1912c9d5c1d78c4
Paul Komkoff <i@stingr.net>
2019-07-04 19:32:18 +0100

    ...

--- storage/storage.go
+++ storage/storage.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"fmt"
+	"io"
 	"strings"
 
 	"github.com/contester/runlib/contester_proto"
@@ -11,6 +12,13 @@ type Backend interface {
 	String() string
 	Copy(localName, remoteName string, toRemote bool, checksum, moduleType, authToken string) (stat *contester_proto.FileStat, err error)
 	Close()
+
+	ReadRemote(name, authToken string) (*RemoteFile, error)
+}
+
+type RemoteFile struct {
+	Stat contester_proto.FileStat
+	Body io.ReadCloser
 }
 
 type statelessBackend struct{}
@@ -22,6 +30,10 @@ func (s statelessBackend) String() string {
 }
 
 func (s statelessBackend) Close() {}
+
+func (s statelessBackend) ReadRemote(name, authToken string) (*RemoteFile, error) {
+	return filerReadRemote(name, authToken)
+}
 
 func (s statelessBackend) Copy(localName, remoteName string, toRemote bool, checksum, moduleType, authToken string) (stat *contester_proto.FileStat, err error) {
 	if fr := isFilerRemote(remoteName); fr != "" {

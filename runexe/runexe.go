195726089bf9349cf80bc1f6edfea588017a59f2
Paul Komkoff <i@stingr.net>
2021-09-03 00:09:27 +0400

    Parse interactor command line a bit more intelligently.

--- runexe/runexe.go
+++ runexe/runexe.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 	"sync"
 
 	"github.com/contester/runlib/platform"
@@ -272,9 +271,9 @@ func main() {
 	var interactorFlags *processConfig
 
 	if globalFlags.Interactor != "" {
-		interactorFlags, _, err = ParseFlags(false, strings.Split(globalFlags.Interactor, " "))
+		interactorFlags, _, err = ParseFlags(false, commandLineToArgv(globalFlags.Interactor))
 		if err != nil {
-			Fail(err, "Parse interator flags")
+			Fail(err, "Parse interactor flags")
 		}
 	}
 
@@ -353,4 +352,69 @@ func main() {
 	if globalFlags.ReturnExitCode {
 		os.Exit(programReturnCode)
 	}
+}
+
+// appendBSBytes appends n '\\' bytes to b and returns the resulting slice.
+func appendBSBytes(b []byte, n int) []byte {
+	for ; n > 0; n-- {
+		b = append(b, '\\')
+	}
+	return b
+}
+
+// readNextArg splits command line string cmd into next
+// argument and command line remainder.
+func readNextArg(cmd string) (arg []byte, rest string) {
+	var b []byte
+	var inquote bool
+	var nslash int
+	for ; len(cmd) > 0; cmd = cmd[1:] {
+		c := cmd[0]
+		switch c {
+		case ' ', '\t':
+			if !inquote {
+				return appendBSBytes(b, nslash), cmd[1:]
+			}
+		case '"':
+			b = appendBSBytes(b, nslash/2)
+			if nslash%2 == 0 {
+				// use "Prior to 2008" rule from
+				// http://daviddeley.com/autohotkey/parameters/parameters.htm
+				// section 5.2 to deal with double double quotes
+				if inquote && len(cmd) > 1 && cmd[1] == '"' {
+					b = append(b, c)
+					cmd = cmd[1:]
+				}
+				inquote = !inquote
+			} else {
+				b = append(b, c)
+			}
+			nslash = 0
+			continue
+		case '\\':
+			nslash++
+			continue
+		}
+		b = appendBSBytes(b, nslash)
+		nslash = 0
+		b = append(b, c)
+	}
+	return appendBSBytes(b, nslash), ""
+}
+
+// commandLineToArgv splits a command line into individual argument
+// strings, following the Windows conventions documented
+// at http://daviddeley.com/autohotkey/parameters/parameters.htm#WINARGV
+func commandLineToArgv(cmd string) []string {
+	var args []string
+	for len(cmd) > 0 {
+		if cmd[0] == ' ' || cmd[0] == '\t' {
+			cmd = cmd[1:]
+			continue
+		}
+		var arg []byte
+		arg, cmd = readNextArg(cmd)
+		args = append(args, string(arg))
+	}
+	return args
 }

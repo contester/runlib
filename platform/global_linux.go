687b940957e605247a94850fd20becc431120f44
Paul Komkoff <i@stingr.net>
2022-10-19 22:38:11 +0100

    Temporary hack to allow linux builds.

--- platform/global_linux.go
+++ platform/global_linux.go
@@ -8,3 +8,7 @@ type ContesterDesktop struct {
 func CreateGlobalData(opts bool) (*GlobalData, error) {
 	return nil, nil
 }
+
+func (s *GlobalData) GetLoadLibraryW32() (uintptr, error) {
+	return 0, nil
+}
